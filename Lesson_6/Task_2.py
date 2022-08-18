036fb87e58c0ebc1aff46646e8b88ce112a79667
Sviatoslav <95108031+svybu@users.noreply.github.com>
2022-08-18 14:28:21 +0300

    Delete Lesson_6 directory

--- Lesson_6/Task_2.py
+++
@@ -1,6 +0,0 @@
-"""4. Определить, какое число в массиве встречается чаще всего."""
-from collections import Counter
-from random import randint
-first_arr = [randint(0, 100) for i in range(100) ] #массив в котором будем искать
-a = Counter(first_arr).most_common(1)
-print(f' Чаще всего встречаеться число {a} раза')
